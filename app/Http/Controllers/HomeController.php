b0873607b48398670143846eb71b4b17371bd550
mzfa <zulfikarfebri1505@gmail.com>
2023-07-04 14:12:24 +0700

    homee dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 
 class HomeController extends Controller
 {
     public function index(){
         // dd(Session::get('role'));
-        return view('dashboard');
+        $project = DB::table('projects')->count();
+        $user = DB::table('users')->count();
+        $task = DB::table('project_test')->count();
+        $uat = DB::table('project_detail_uat')->count();
+        return view('dashboard', compact('project','user','task','uat'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,25 @@
 @extends('layouts.app')
 @section('content')
 <div class="row">
+    <div class="col-12">
+        <div class="card card-block card-stretch card-height bg-info">
+            <div class="card-body">
+                <div class="top-block d-flex align-items-center justify-content-between text-white">
+                    <h5 class="text-white">Selamat Datang, {{ Auth::user()->name }} </h5>
+                    <span class="badge badge-primary"></span>
+                </div>
+                <h3 class="text-white">Di Aplikasi Management Projek</h3>
+            </div>
+        </div>
+    </div>
     <div class="col-md-6 col-lg-3">
         <div class="card card-block card-stretch card-height">
             <div class="card-body">
                 <div class="top-block d-flex align-items-center justify-content-between">
                     <h5>Projek</h5>
-                    <span class="badge badge-primary">Monthly</span>
+                    <span class="badge badge-primary"></span>
                 </div>
-                <h3><span class="counter">35000</span></h3>
+                <h3><span class="counter">{{ $project }}</span></h3>
             </div>
         </div>
     </div>
@@ -17,9 +28,9 @@
             <div class="card-body">
                 <div class="top-block d-flex align-items-center justify-content-between">
                     <h5>User</h5>
-                    <span class="badge badge-warning">Anual</span>
+                    <span class="badge badge-warning"></span>
                 </div>
-                <h3><span class="counter">25100</span></h3>
+                <h3><span class="counter">{{ $user }}</span></h3>
             </div>
         </div>
     </div>
@@ -28,9 +39,9 @@
             <div class="card-body">
                 <div class="top-block d-flex align-items-center justify-content-between">
                     <h5>Task</h5>
-                    <span class="badge badge-success">Today</span>
+                    <span class="badge badge-success"></span>
                 </div>
-                <h3><span class="counter">33000</span></h3>
+                <h3><span class="counter">{{ $task }}</span></h3>
             </div>
         </div>
     </div>
@@ -38,10 +49,9 @@
         <div class="card card-block card-stretch card-height">
             <div class="card-body">
                 <div class="top-block d-flex align-items-center justify-content-between">
-                    <h5>Profit</h5>
-                    <span class="badge badge-info">Testing</span>
+                    <h5>UAT</h5>
                 </div>
-                <h3><span class="counter">2500</span></h3>
+                <h3><span class="counter">{{ $uat }}</span></h3>
             </div>
         </div>
     </div>
